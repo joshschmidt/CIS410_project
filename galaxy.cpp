d434f7e16a07327377b91404570dabb1b7e2560b
Mikey Knowles <mikey@ubuntu.(none)>
2014-05-13 21:07:43 -0700

    bugfix

--- galaxy.cpp
+++ galaxy.cpp
@@ -1,7 +1,7 @@
 #include "galaxy.h"    
 
 
-Galaxy::Galaxy(int seed, int planetCount) {
+Galaxy::Galaxy(int seed, int id, int planetCount) {
 	cilk_for(int i = 0; i < planetCount; i++) {
 			_planets[i] = Planet(seed, i);
 		}
