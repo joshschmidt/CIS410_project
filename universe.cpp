ae254519ffbdd47e8d9943a5bb215fb87f0db591
Mikey Knowles <mikey@ubuntu.(none)>
2014-05-15 23:27:06 -0700

    bugfix

--- universe.cpp
+++ universe.cpp
@@ -6,13 +6,13 @@ Universe::Universe(int seed, int id, int length, int width, int height) {
 	_id = id;
 	_length = length;
 	_width = width;
-	_height = height;git p
+	_height = height;
 	_galaxies = new Galaxy*[length]; 
 
 	// Create 2D array of pointers:
-	_galaxies = new (int**)[rows];
+	_galaxies = new (Galaxy**)[length];
 	for (int i = 0; i < length; ++i) {
-  		_galaxies[i] = new (Galaxy*)[width];
+  		_galaxies[i] = new (Galaxy**)[width];
 	}
 
 	for(int x = 0; x < length; x++) {
