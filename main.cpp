4ec44f1dab431f5e5de88d87d9a0966803f64795
Marc Leef <mleef@uoregon.edu>
2014-05-20 11:54:38 -0700

    Removed unnecessary queue class and added event queue to main

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include "universe.h"
 #include <stdio.h>      
 #include <stdlib.h> 
+#include <iostream>
+#include <queue>
+
+struct Event {
+	float time;
+}
+
+class TimeCompare {
+	public:
+		bool operator()(Event& e1, Event& e2) {
+			if (e1.time < e2.time) return true;
+			return false;
+		}
+
+};
 
 
 int main() {
 	Universe universe = Universe(1, 1, 5, 5, 5);
+	priority_queue<Event, vector<Event>, TimeCompare> pq
 }
