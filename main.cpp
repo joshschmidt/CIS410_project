0f9dca17d1834aaeed8653330851700f8cdd5be4
Mikey Knowles <mikey@ubuntu.(none)>
2014-05-30 07:57:48 -0700

    fix paths

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
-#include "../Universe/universe.h"
-#include "../Galaxy/galaxyManager.h"
-#include "../Event/event.h"
+#include "/Universe/universe.h"
+#include "/Galaxy/galaxyManager.h"
+#include "/Event/event.h"
 #include <stdio.h>      
 #include <stdlib.h> 
 #include <iostream>
